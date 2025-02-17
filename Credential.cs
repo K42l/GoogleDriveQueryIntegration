b8a2a2e7f0c1455e83b59bb8c13f7934369b5656
klino <klino@caasp.org.br>
2025-02-17 11:55:25 -0300

    Add a credential extension method to IServiceCollecttion

---
+++ Credential.cs
@@ -0,0 +1,29 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Drive.Query.Integration;
+using Google.Drive.Query.Integration.Interface;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Google.Drive.Backup.Util
+{
+    public static class Credential
+    {
+        public static IServiceCollection AddIntegration(this IServiceCollection services, string applicationName) 
+        {
+            GoogleCredential credential = GoogleCredential.GetApplicationDefault()
+                    .CreateScoped(DriveService.Scope.Drive);
+
+            var dService = new DriveService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = applicationName
+            });
+
+            return services.AddSingleton<IIntegration, Integration>(x =>
+            {
+                return new Integration(dService);
+            });
+        }
+    }
+}
